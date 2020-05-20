eda3c87c02add6d943700a10cfddb841c279a47d
Deepanshu Tiwari <deepanshutiwari00@gmail.com>
2020-05-20 14:52:51 +0530

    minor update

---
+++ build/precache-manifest.fe9f881f02c1da96a6d6dcb564994e14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bc2423e17d1a05557849c6e14696c46",
+    "url": "/http//Deepanshu-Tiwari.github.io/Covid-19-Tracker-Application/index.html"
+  },
+  {
+    "revision": "e4cd9da89d778b0b7671",
+    "url": "/http//Deepanshu-Tiwari.github.io/Covid-19-Tracker-Application/static/css/main.41268c43.chunk.css"
+  },
+  {
+    "revision": "8c71069f14de4e40ed37",
+    "url": "/http//Deepanshu-Tiwari.github.io/Covid-19-Tracker-Application/static/js/2.0e321de2.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/http//Deepanshu-Tiwari.github.io/Covid-19-Tracker-Application/static/js/2.0e321de2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4cd9da89d778b0b7671",
+    "url": "/http//Deepanshu-Tiwari.github.io/Covid-19-Tracker-Application/static/js/main.0ea1561e.chunk.js"
+  },
+  {
+    "revision": "359ba195d9340a091240",
+    "url": "/http//Deepanshu-Tiwari.github.io/Covid-19-Tracker-Application/static/js/runtime-main.177f0152.js"
+  }
+]);
